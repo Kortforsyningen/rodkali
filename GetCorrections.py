415ac436ca23219cb05536c14f164f5f80875304
Simon Kokkendorff <silyko@gmail.com>
2012-06-04 14:50:53 +0200

    Almost done - works on unix

--- GetCorrections.py
+++ GetCorrections.py
@@ -1,226 +1,334 @@
+#!/usr/bin/python 
+##################
+##GetCorrections.py simlk, june 2012
+##kald: GetCorrections.py <input_files> <output_dir>
+################
 import os, sys
-import numpy as np
 import glob
-import matplotlib.pyplot as plt
+from math import *
+import time
 
+DEBUG=False
+#ROD = [l_0 , m_0 , alpha_T, v_G, T_0]   - see calibration report
+
+ROD_53274=[-0.038*1e-3 , 7.36 , 0.72 , 0.001*1e-3 , 20.1]
+ROD_53273=[-0.038*1e-3 , 7.36 , 0.72 , 0.001*1e-3 , 20.1] 
+
+
+class Rod(object):
+	def __init__(self,name,data=None,zeroshift=None):
+		self.name=name
+		if data is not None:
+			self.l0=data[0]
+			self.m0=data[1]
+			self.alpha_t=data[2]
+			self.vg=data[3]
+			self.t0=data[4]
+			self.is_calibrated=True
+		else:
+			self.is_calibrated=False
+		if zeroshift is not None:
+			self.zeroshift=zeroshift
+		else:
+			self.zeroshift=None
+	def Correct(self,temp,h):
+		if self.zeroshift is None:
+			raise Exception("Warning: rod %s, zeroshift not set!!")
+		else:
+			zs=self.zeroshift
+		h_real=h-zs
+		h_corr=self.l0+h_real*(1+(self.m0+self.alpha_t*(temp-self.t0))*1e-6)+self.vg
+		h_corr+=zs
+		if DEBUG:
+			print("Rod: %s, before: %.7f m, after: %.7f m, zeroshift: %.4f m" %(self.name,h,h_corr,zs))
+			s=raw_input(":")
+			if "stop" in s:
+				sys.exit()
+		return h_corr
+	def StandardCorrection(self,temp,h):
+		if self.zeroshift is None:
+			raise Exception("Warning: rod %s, zeroshift not set!!")
+		else:
+			zs=self.zeroshift
+		h_real=h_zs
+		h_corr=h_real*(1+(0.8*1e-6)*(temp-20.0))
+		h_corr+=zs
+		return h_corr
+		
+	def SetZeroshift(self,zs):
+		self.zeroshift=zs
+		
 #TRANSLATION TABLE#
-RODS={"110":"53274.txt",
-"111":"53274.txt",
-"112":"53274.txt",
-"961":"53274.txt",
-"962":"53274.txt",
-"963":"53274.txt",
-"964":"53274.txt",
-"965":"53274.txt",
-"966":"53274.txt",
-"967":"53274.txt",
-"968":"53274.txt"
+RODS={
+"110":  Rod("110",ROD_53274),
+"111":  Rod("111",ROD_53273),
+"112":  Rod("112",ROD_53273),
+"961":  Rod("961",ROD_53273),
+"962":  Rod("962",ROD_53273),
+"963":  Rod("963",ROD_53273),
+"964":  Rod("963",ROD_53273),
+"965":  Rod("965",ROD_53273),
+"966":  Rod("966",ROD_53273),
+"967":  Rod("967",ROD_53273),
+"968":  Rod("968",ROD_53273)
 }
 
+ND_VALUE=-999
 
-def GetCorrections(f):
-	t_expansion=None
-	cor=[]
-	for line in f:
-		sline=line.split()
-		if len(sline)==0:
-			continue
-		if sline[0]=="#":
-			rodname=sline[1]
-			temp=float(sline[2])
-			t_expansion=float(sline[3])
-			s_const=float(sline[4])
-		else:
-			try:
-				m=float(sline[0])
-				c=float(sline[1])
-			except:
-				pass
-			else:
-				cor.append([m,c])
-	return rodname,cor,temp,t_expansion,s_const
-	
-
-class RodCorrector(object):
-	def __init__(self,fname):
-		f=open(fname)
-		self.rod,self.cor,self.temp,self.t_exp,self.s_cont=GetCorrections(f)
-		f.close()
-		self.cor=np.array(self.cor)
-		self.done=0
-		self.mean_marks=0
-	def Correct(self,temp,h,zs):
-		self.done+=1
-		reading=h-zs
-		#find nearest marks#
-		test=np.fabs(self.cor[:,0]-reading)
-		I=np.where(test<0.15)[0]
-		cor=0
-		for j in I:
-			cor+=self.cor[j,1]*(1.0+self.t_exp*(temp-self.temp))/I.size
-		new=h+cor
-		self.mean_marks=self.mean_marks*(self.done-1)/float(self.done)+I.size/float(self.done)
-		return new
-
-
-def GetData(f,rods):
+###############################
+## Function that reads data from data-file
+## This can be complicated due to 'hand-editing' of file!!!
+###############################
+def GetData(f,rods,n_lines):
 	stretches=[]
 	line=f.readline()
 	stretch=Stretch()
 	msg=""
+	p1=None
+	p2=None
+	should_end=False
 	while len(line)>0:
+		n_lines+=1
 		sline=line.split()
-		if "tilbagesigte" in line:
-			if len(sline)>7:
+		if len(sline)==0:
+			line=f.readline()
+			continue
+		if "tilbagesigte"==sline[0]:
+			#len can be 6, 7, 8 or 9
+			# 6 and 7 means single meas, 8 and 9 means double
+			# 7 and 9 means start of new stretch!
+			if should_end:
+				msg+="We seem to have a deleted head around line %d\n" %n_lines
+			if len(sline)>=8:
 				data=[float(sline[-2]),float(sline[-4])]
 				rod=sline[-7]
 			else:
 				data=[float(sline[-2])]
 				rod=sline[-5]
-			if rods.has_key(rod):
-				zs=rods[rod]
+			if len(sline)==7 or len(sline)==9:
+				start_new=True
+				stretch=Stretch()
+				p1=sline[1]
+			if rod in rods:
+				rod_class=rods[rod]
 			else:
-				zs=0
-				msg+="Zeroshift of rod %s not found!\n" %rod
-			stretch.AddBack(data,zs,rod)
-		elif "fremsigte" in line:
-			if len(sline)>9:
+				rod_class=None
+				msg+="Rod %s not defined!\n" %rod
+			stretch.AddBack(data,rod_class)
+		elif "fremsigte"==sline[0]:
+			#len can be 8 9 10 or 11 - 9 or 11 means we have an endpoint
+			#8,9 means single meas, 10 or 11 means double meas.
+			if len(sline)>=10:
 				data=[float(sline[-6]),float(sline[-4])]
-				
 				rod=sline[-9]
 			else:
 				data=[float(sline[-4])]
 				rod=sline[-7]
-			if rods.has_key(rod):
-				zs=rods[rod]
+			if len(sline)==9 or len(sline)==11:
+				should_end=True
+				p2=sline[1]
 			else:
-				zs=0
-				msg+="Zeroshift of rod %s not found!\n" %rod
-			stretch.AddForward(data,zs,rod)	
+				should_end=False
+			if rod in rods:
+				rod_class=rods[rod]
+			else:
+				rod_class=None
+				msg+="Rod %s not defined!\n" %rod
+			stretch.AddForward(data,rod_class)	
 		elif len(sline)>0 and sline[0]=="T:":
 			stretch.AddTemp(float(sline[1]))
 		elif len(sline)>0 and sline[0]=="#":
-			stretch.AddTemp(float(sline[8]))
-			stretch.SetPoints(sline[1],sline[2])
-			stretch.SetDistance(float(sline[5]))
+			if not should_end:
+				msg+="line: %d, %s -> %s, something wrong in data-formatting!\n"%(n_lines,sline[1],sline[2])
+			should_end=False
+			if p1!=sline[1] or p2!=sline[2]:
+				msg+="line: %d, head says: %s -> %s, data says: %s -> %s\n" %(n_lines,sline[1],sline[2],p1,p2)
+			stretch.SetHead(sline)
+			#if head found - save and test (in stretch class)
 			stretches.append(stretch)
-		
-			stretch=Stretch()
 		line=f.readline()
+		
 	return stretches,msg
 	
 class Stretch(object):
 	def __init__(self):
-		self.back=np.empty((0,3))
-		self.forward=np.empty((0,3))
-		self.temp=np.empty((0,))
+		self.back=[]
+		self.forward=[]
+		self.temp=[]
 		self.p1=None
 		self.p2=None
 		self.distance=0
 		self.forward_rods=[]
 		self.back_rods=[]
-		
-	def SetPoints(self,p1,p2):
-		self.p1=p1
-		self.p2=p2
-	def SetDistance(self,d):
-		self.distance=d
+		self.all_double=True
+		self.some_double=False
+		self.head=None
+	def SetHead(self,head):
+		self.head=head
+		self.AddTemp(float(head[8]))
+		self.p1=head[1]
+		self.p2=head[2]
+		self.distance=float(head[5])
+		if len(head)>9:
+			self.nopst=int(head[-1])
+			if self.nopst!=len(self.forward):
+				raise Exception("%s->%s. Number of setups in head incosistent with data!" %(self.p1,self.p2))
+		self.raw_hdiff=float(head[6])
+		diff=abs(self.raw_hdiff-self.GetHdiff())
+		if diff>1e-4:
+			h1=sum([h[0] for h in self.forward])
+			h2=sum([h[0] for h in self.back])
+			print("Calulated from single meas: %.6f, saved: %.6f" %(h2-h1,self.raw_hdiff))
+			raise Exception("%s->%s. Inconsistency %.4f mm between calculated hdiff and hdiff from head" 
+			%(self.p1,self.p2,diff*1000))
+			
 	def GetPoints(self):
 		return self.p1,self.p2
-	def AddBack(self,data,zs,rod):
+	def AddBack(self,data,rod):
 		if len(data)==1:
-			data.append(-999)
-		data.append(zs)
-		self.back=np.vstack((self.back,data))
-		self.back_rods.append(rod)
-	def AddForward(self,data,zs,rod):
-		if len(data)==1:
-			data.append(-999)
-		data.append(zs)
-		self.forward=np.vstack((self.forward,data))
-		self.forward_rods.append(rod)
-	def AddTemp(self,t):
-		self.temp=np.append(self.temp,[t]*(self.back.shape[0]-self.temp.shape[0]))
-	def ApplyCorrection(self,rods=None):
-		new_back=np.ones_like(self.back)*-999
-		new_forward=np.ones_like(self.forward)*-999
-		if (self.back[:,1]<0).any():
-			do=1
+			data.append(ND_VALUE)
+			self.all_double=False
 		else:
+			self.some_double=True
+		self.back.append(data)
+		self.back_rods.append(rod)
+	def AddForward(self,data,rod):
+		if len(data)==1:
+			data.append(ND_VALUE)
+		self.forward.append(data)
+		self.forward_rods.append(rod)
+	#check that temps are connected to right measurement!#
+	def AddTemp(self,t):
+		if len(self.temp)<len(self.back):
+			lots_of_temps=[t]*(len(self.back)-len(self.temp))
+			self.temp.extend(lots_of_temps)
+	def ApplyCorrection(self):
+		ok=True
+		new_back=[]
+		new_forward=[]
+		if self.some_double:			
 			do=2
-		msg=""
-		func_forward=None
-		func_back=None
-		for i in range(do):
-			for j in range(self.back.shape[0]):
-				frod=self.forward_rods[j]
-				brod=self.back_rods[j]
-				if rods is not None:
-					if frod in rods:
-						func_forward=rods[frod].Correct
-					if brod in rods:
-						func_back=rods[frod].Correct
-				if func_forward is None:
-					msg+="%s to %s, setup: %d, rod %s not found. Using standad correction.\n" %(self.p1,self.p2,j+1,frod)
-					func_forward=StandardCorrection
-				if func_back is None:
-					msg+="%s to %s, setup: %d, rod %s not found. Using standad correction.\n" %(self.p1,self.p2,j+1,brod)
-					func_back=StandardCorrection
-				new_back[j,i]=func_back(self.temp[j],self.back[j,i],self.back[j,2])
-				new_forward[j,i]=func_forward(self.temp[j],self.forward[j,i],self.forward[j,2])
-		return new_back,new_forward,msg
+		else:
+			do=1
+		msg="Stretch: %s to %s\n" %(self.p1,self.p2)
+		if self.some_double and not self.all_double:
+			msg="Weirdness: Some double-double (precision) measured, but not all!\n"
+			ok=False
+		for j in range(len(self.back)):
+			f_data=[]
+			b_data=[]
+			frod=self.forward_rods[j]
+			brod=self.back_rods[j]
+			if frod.is_calibrated:
+				func_forward=frod.Correct
+			else:
+				msg+="setup: %d, rod not calibrated. Using standard correction.\n" %(j+1)
+				func_forward=frod.StandardCorrection
+				ok=False
+			if brod.is_calibrated:
+				func_back=brod.Correct
+			else:
+				msg+="setup: %d, rod not calibrated. Using standad correction.\n" %(j+1)
+				func_back=brod.StandardCorrection
+				ok=False
+			for i in range(do):
+				if (self.back[j][i]==ND_VALUE):
+					f_data.append(ND_VALUE)
+					b_data.append(ND_VALUE)
+					continue
+				#print("Back:")
+				val=func_back(self.temp[j],self.back[j][i])
+				b_data.append(val)
+				#print val,new_back
+				#print("Forw:")
+				val=func_forward(self.temp[j],self.forward[j][i])
+				f_data.append(val)
+				#print val,new_forward
+			new_forward.append(f_data)
+			new_back.append(b_data)
+		#print("back:\n%s\n%s\nforw:\n%s\n%s" %(new_back,self.back,new_forward,self.forward))
+		return new_back,new_forward,msg,ok
 		
 	def GetHdiff(self,back=None,forw=None):
 		if back is None:
 			back=self.back
 			forw=self.forward
-		if back.shape!=forw.shape:
-			print("Uoops, mismatching shapes!")
-			return -999
-		if (back[:,1]<0).any():
-			do=2
-		else:
-			do=1
-		hdiff=back[:,:do].mean()-forw[:,:do].mean()
-		return hdiff
+		if len(back)!=len(forw):
+			raise Exception("Uoops, mismatching shapes! Wrong formatting in data file")
+		dh=0
+		for i in range(len(forw)):
+			dh_setup=0
+			if self.some_double and (forw[i][1]!=ND_VALUE and back[i][1]!=ND_VALUE):
+				dh_setup=(back[i][0]+back[i][1]-(forw[i][0]+forw[i][1]))*0.5
+			else:
+				dh_setup=(back[i][0]-forw[i][0])
+			dh+=dh_setup
+		return dh
 			
 		
-def StandardCorrection(temp,h,zs):
+def StandardCorrection(temp,h,zs=0):
 	new=((h-zs)*(1.000003+0.83*(10**-6)*(temp-20))+zs)
 	return new
 
-def GetRods(f):
-	rods=dict()
+################################
+# Gets rods from data-file and copies header to output#
+################################
+
+	
+def GetRods(f,out):
+	rods=[]
+	n_lines=0
 	line=f.readline()
 	while len(line)>0:
+		n_lines+=1
+		out.write(line)
 		sline=line.split()
 		if len(sline)==0:
 			line=f.readline()
 			continue
+		#Check for formatting signal that header ended:
 		if sline[0]=="*":
-			return rods
+			return rods,n_lines
 		if len(sline)==4 and "nulpunktsfejl" in line.lower():
-			rods[sline[0][:-1]]=float(sline[-2])
+			rods.append([sline[0][:-1],float(sline[-2])])
 		line=f.readline()
-	return rods
+	return None,None
+
+def GetDiff(l1,l2):
+	if len(l1)!=len(l2):
+		return None
+	diff=[l1-l2 for l1,l2 in zip(l1,l2)]
+	return diff
+
+def GetStats(data):
+	sd=None
+	n=len(data)
+	m=sum(data)/float(n)
+	if n>1:
+		sq_dev=sum([(x-m)**2/(n-1) for x in data])
+		sd=sqrt(sq_dev)
+	return m,sd
+		
 	
+	
+def Usage():
+	print("To run:")
+	print("%s <input_files> <output_dir>" %os.path.basename(sys.argv[0]))
+	sys.exit()
 
 def main(args):
-	#f=open(args[1])
-	#cor,t_exp,s_const=GetCorrections(f)
-	#f.close()
+	if len(args)<3:
+		Usage()
 	files=glob.glob(args[1])
+	outdir=args[2]
 	ndiffs=[]
 	diffs=[]
 	ndiffs2=[]
 	diffs2=[]
-	print("Setting up rod translations:")
-	rod_translations={}
-	for rod in RODS.keys():
-		print("%s is %s." %(rod,RODS[rod]))
-		rod_translations[rod]=RodCorrector(RODS[rod])
+	if not os.path.exists(outdir):
+		os.mkdir(outdir)
 	for fname in files:
-		
+		out=None
 		try:
 			f=open(fname)
 		except:
@@ -228,59 +336,78 @@ def main(args):
 		line=f.readline()
 		if "MGL" in line:
 			print("Reading %s" %fname)
-			rods=GetRods(f)
-			stretches,msg=GetData(f,rods)
+			base,ext=os.path.splitext(os.path.basename(fname))
+			outname=os.path.join(outdir,base+"_corr"+ext)
+			try:
+				out=open(outname,"w")
+			except:
+				print("Could not open outfile %s." %outname)
+				return -1
+			rods,n_lines=GetRods(f,out)
+			out.write("; Corrections caluculated at %s\n" %time.asctime())
+			for rod,zs in rods:
+				if rod in RODS:
+					RODS[rod].SetZeroshift(zs)
+				else:
+					print("Warning: rod %s not globally defined!" %rod)
+					print("Using a standard correction!")
+					RODS[rod]=Rod(rod,zeroshift=zs)
+			try:
+				stretches,msg=GetData(f,RODS,n_lines)
+			except Exception,msg:
+				print(repr(msg))
+				print("This is probably something serious (wrong rod, wrong zeroshift) etc. Check data!")
+				print("No ouput - continuing....")
+				out.close()
+				continue
 			if len(msg)>0:
 				print msg
 			print("Got %d stretches" %len(stretches))
 			for s in stretches:
-				bc,fc,msg=s.ApplyCorrection()
-				bc1,fc1,msg=s.ApplyCorrection(rod_translations)
-				if len(msg)>0:
+				bc,fc,msg,ok=s.ApplyCorrection()
+				if not ok:
 					print msg
-				h1,h2,h3=s.GetHdiff(),s.GetHdiff(bc,fc),s.GetHdiff(bc1,fc1)
-				diff=(h1-h2)*1000
-				diff2=(h1-h3)*1000.0
-				if abs(diff)>np.sqrt(s.distance/1000.0)*0.5:
-					p1,p2=s.GetPoints()
+				h_uncorr,h_corr=s.GetHdiff(),s.GetHdiff(bc,fc)
+				diff=abs(h_corr-h_uncorr)*1000
+				p1,p2=s.GetPoints()
+				if diff>sqrt(s.distance/1000.0)*0.2:
+					mean_temp=sum(s.temp)/float(len(s.temp))
+					print("Large correction!")
 					print("file: %s, stretch: %s->%s, diff: %.4f mm, dist: %.2f m" %(fname,p1,p2,diff,s.distance))
-					print("Without: %.6f m, with: %.6fm" %(h1,h2))
-					print("Mean-temp: %.2f deg C" %s.temp.mean())
-				ndiffs.append(abs(diff)/np.sqrt(s.distance/1000.0))
+					print("raw: %.6f m, corr: %.6fm" %(h_uncorr,h_corr))
+					print("normalised: %.6f ne" %(diff/sqrt(s.distance/1000.0)))
+					print("Mean-temp: %.2f deg C" %mean_temp)
+				ndiffs.append(diff/sqrt(s.distance/1000.0))
 				diffs.append(diff)
-				ndiffs2.append(abs(diff2)/np.sqrt(s.distance/1000.0))
-				diffs2.append(diff2)
+				if True: #perhaps add some switch here for no file output....
+					out.write("; %s to %s\n" %(p1,p2))
+					out.write("; ")
+					head=s.head
+					for token in head:
+						out.write(token+" ")
+					head[6]="%.7f" %h_corr
+					out.write("\n")
+					for token in head:
+						out.write(token+" ")
+					out.write("\n")
+			out.close()
 		else:
 			print("%s not (new) MGL file" %fname)
 		f.close()
-	print("Mean diff: %.6f mm" %np.mean(diffs))
-	for rod in rod_translations.keys():
-		C=rod_translations[rod]
-		if C.done>0:
-			print("Mean number of marks for rod %s: %.3f" %(rod,C.mean_marks))
-	plt.subplot(2,2,1)
-	plt.title("Afstandsnormaliserede forskelle - bedste rette linie")
-	plt.hist(ndiffs)
-	plt.xlabel("mm/sqrt(km)")
-	plt.subplot(2,2,2)
-	plt.title("Forkskelle - bedste rette linie")
-	plt.xlabel("mm")
-	plt.hist(diffs)
-	plt.subplot(2,2,3)
-	plt.title(u"Afstandsnormaliserede forskelle - med m\u00E6rker")
-	plt.hist(ndiffs2)
-	plt.xlabel("mm/sqrt(km)")
-	plt.subplot(2,2,4)
-	plt.title(u"Forkskelle - med m\u00E6rker")
-	plt.hist(diffs2)
-	plt.xlabel("mm")
-	plt.figure()
-	plt.title("Rod: %s" %C.rod)
-	plt.plot(C.cor[:,0],C.cor[:,1]*10**6)
-	plt.xlabel("Mark [m]")
-	plt.ylabel("Correction [um]")
-	plt.show()
-
+		
+	m_diff,sd_diff=GetStats(diffs)
+	m_norm,sd_norm=GetStats(ndiffs)
+	max_diff=max(map(abs,diffs))
+	max_ne=max(map(abs,ndiffs))
+	print("Number of hdiffs:     %d" %len(diffs))
+	print("Mean (abs) diff:      %.6f mm" %m_diff)
+	print("Std-dev  :            %.6f mm" %sd_diff)
+	print("Max      :            %.6f mm" %max_diff)
+	print("Mean normalized diff: %.6f ne" %m_norm)
+	print("Std-dev  :            %.6f ne" %sd_norm)
+	print("Max      :            %.6f ne" %max_ne)
+	return 
+	
 if __name__=="__main__":
-	main(sys.argv)
+	sys.exit(main(sys.argv))
 	
