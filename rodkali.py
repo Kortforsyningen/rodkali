c2634f42929859096f1d957f9ab42b1416c22abe
Simon Kokkendorff <silyko@gmail.com>
2012-09-24 10:48:34 +0200

    ok - fixed small bug

--- rodkali.py
+++ rodkali.py
@@ -365,7 +365,7 @@ def GetStats(data):
 def Usage():
 	sys.stdout=sys.__stdout__
 	PROGNAME=os.path.basename(sys.argv[0])
-	print("%s version: %s", %(PROGNAME,VERSION))
+	print("%s version: %s" %(PROGNAME,VERSION))
 	print("To run:")
 	print("%s <input_files> <output_dir>" %PROGNAME)
 	sys.exit()
